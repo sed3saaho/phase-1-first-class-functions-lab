8052490623ef84ee800374d632bee613194d4002
Sedly <sedly.keith@student.moringaschool.com>
2024-04-12 03:53:11 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+// Function selectDifferentDrivers that returns either the first two or last two drivers based on the function argument provided
+const selectDifferentDrivers = function(drivers, selectionFunction) {
+    return selectionFunction(drivers);
+};
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
